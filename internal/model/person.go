36ec2a56f9fab1527b55195dc61802fac196dc41
Mehdi Teymorian <mehditeymorian322@gmail.com>
2022-12-06 11:01:19 +0330

    feat: remove unnecessary attribute

--- internal/model/person.go
+++ internal/model/person.go
@@ -1,15 +1,13 @@
 package model
 
 type Person struct {
-	Name         string
-	Age          uint
-	MoneyBalance uint
+	Name string
+	Age  uint
 }
 
 func NewPerson(name string, age uint) Person {
 	return Person{
-		Name:         name,
-		Age:          age,
-		MoneyBalance: 0,
+		Name: name,
+		Age:  age,
 	}
 }
