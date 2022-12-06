7def9cec6b01691de5b96955ad1155640c421127
Mehdi Teymorian <mehditeymorian322@gmail.com>
2022-12-06 10:51:57 +0330

    feat: add counter for ids

--- internal/db/db.go
+++ internal/db/db.go
@@ -3,11 +3,13 @@ package db
 import "example-basics/internal/model"
 
 type Database struct {
-	people map[uint]model.Person
+	people  map[uint]model.Person
+	counter uint
 }
 
 func NewDatabase() Database {
 	return Database{
-		people: make(map[uint]model.Person),
+		people:  make(map[uint]model.Person),
+		counter: 0,
 	}
 }
