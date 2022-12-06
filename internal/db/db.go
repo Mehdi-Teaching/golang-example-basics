fadcf06c45ff5cac130574d4c37ede3754dcef03
Mehdi Teymorian <mehditeymorian322@gmail.com>
2022-12-06 11:00:46 +0330

    feat: complete add, remove, print, and get all functionalities

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,6 +1,9 @@
 package db
 
-import "example-basics/internal/model"
+import (
+	"example-basics/internal/model"
+	"fmt"
+)
 
 type Database struct {
 	people  map[uint]model.Person
@@ -12,4 +15,39 @@ func NewDatabase() Database {
 		people:  make(map[uint]model.Person),
 		counter: 0,
 	}
+}
+
+func (d *Database) AddPerson(person model.Person) uint {
+	d.people[d.counter] = person
+
+	id := d.counter
+
+	d.counter++
+
+	return id
+}
+
+func (d *Database) RemovePerson(id uint) bool {
+	_, ok := d.people[id]
+	if !ok {
+		return false
+	}
+
+	delete(d.people, id)
+
+	return true
+}
+
+func (d *Database) GetAll() []model.Person {
+	result := make([]model.Person, 0)
+
+	for _, person := range d.people {
+		result = append(result, person)
+	}
+
+	return result
+}
+
+func (d *Database) PrintAll() {
+	fmt.Printf("%+v\n", d.GetAll())
 }
